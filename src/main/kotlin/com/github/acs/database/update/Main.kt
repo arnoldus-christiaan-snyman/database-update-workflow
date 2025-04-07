f1ae76dafc6846e0e4bae4c70f0eed0dbef6c98b
Arno Snyman <ArnoSnyman007@gmail.com>
2025-04-07 15:15:11 +0200

    initialize project structure with Gradle, add application properties, and configure .gitignore

---
+++ src/main/kotlin/com/github/acs/database/update/Main.kt
@@ -0,0 +1,11 @@
+package com.github.acs.database.update
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Main
+
+fun main(args: Array<String>) {
+	runApplication<Main>(*args)
+}
